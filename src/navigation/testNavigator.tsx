7295dcc01aa643cb2f996a76706d5323a4b3e64c
kate <eunsu.kate@gmail.com>
2024-09-06 10:21:19 +0900

    feat: navigator 테스트

--- src/navigation/testNavigator.tsx
+++ src/navigation/testNavigator.tsx
@@ -1,19 +1,35 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { NavigationContainer } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Text, View } from "react-native"
+import { createStackNavigator } from "@react-navigation/stack";
+import { Button, Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 
+type RootStackParamList = {
+  TabNavi: undefined;
+  ScreenC: undefined;
+  ScreenA: undefined;
+  ScreenB: undefined;
+};
 
+type ScreenANavigationProp = NativeStackNavigationProp<RootStackParamList, 'ScreenA'>;
 
 const Tab = createBottomTabNavigator();
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 
-const ScreenA = () => {
+
+const ScreenA = ({ navigation }: { navigation: ScreenANavigationProp }) => {
   return (
     <SafeAreaView style={tw`flex-1 items-center justify-center`}>
       <Text>Screen A</Text>
+      <Button title="Go to Screen C" onPress={() => {
+        navigation.navigate('ScreenC')
+      }} />
+      <Button title="Go to Screen B" onPress={() => {
+        navigation.navigate('ScreenB')
+      }} />
     </SafeAreaView>
   )
 }
@@ -25,10 +41,21 @@ const ScreenB = () => {
     </SafeAreaView>
   )
 }
+const ScreenC = () => {
+  return (
+    <SafeAreaView style={tw`flex-1 items-center justify-center`}>
+      <Text>Screen C</Text>
+    </SafeAreaView>
+  )
+}
 
 const TabNavi = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={() => {
+      return {
+        headerShown: false
+      }
+    }}>
       <Tab.Screen name="ScreenA" component={ScreenA} />
       <Tab.Screen name="ScreenB" component={ScreenB} />
     </Tab.Navigator>
@@ -37,9 +64,15 @@ const TabNavi = () => {
 
 
 
+
 const TestNavigator = () => {
   return (
-    <TabNavi />
+
+    <Stack.Navigator>
+      <Stack.Screen name="TabNavi" component={TabNavi} />
+      <Stack.Screen name="ScreenC" component={ScreenC} />
+    </Stack.Navigator>
+
   )
 }
 
